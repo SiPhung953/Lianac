186d99130850f3d1252582e1b073de37d9a65f7b
Phung Dam Tien Si <93171516+SiPhung953@users.noreply.github.com>
2025-10-05 11:00:48 +0700

    Updated Setting Fragment
    Matching the new version on Canva

--- app/src/main/java/vn/edu/lianac/MainActivity.java
+++ app/src/main/java/vn/edu/lianac/MainActivity.java
@@ -1,40 +1,20 @@
 package vn.edu.lianac;
 
-import android.content.Context;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-
-    public class DownloadItemDivider extends RecyclerView.ItemDecoration {
-        private final Drawable dDivider;
-        private final int dHeight;
-
-        public DownloadItemDivider(Context context, int heightInPixels) {
-            dDivider = new ColorDrawable(0x1A000000);
-            dHeight = heightInPixels;
-        }
-
-        @Override
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        if (savedInstanceState == null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.fragment_container, new SettingFragment());
+            ft.commit();
+        }
     }
 }
