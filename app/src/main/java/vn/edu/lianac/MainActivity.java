dcd00bbcc5eda94a981ecc81f01114036cf97a7a
Tien24205 <email_dang_ky_github_cua_Tien24205>
2025-10-06 20:27:33 +0700

    SettingsFragment test 2.0

--- app/src/main/java/vn/edu/lianac/MainActivity.java
+++ app/src/main/java/vn/edu/lianac/MainActivity.java
@@ -3,11 +3,16 @@ package vn.edu.lianac;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
-
+import android.content.SharedPreferences;
+import androidx.appcompat.app.AppCompatDelegate;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SharedPreferences prefs = getSharedPreferences("SettingsPrefs", MODE_PRIVATE);
+        int themeMode = prefs.getInt("theme_mode", AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        AppCompatDelegate.setDefaultNightMode(themeMode);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
