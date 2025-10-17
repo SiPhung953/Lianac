52f5281feac182720d9c07b0c3909c8cdd674499
Phung Dam Tien Si <93171516+SiPhung953@users.noreply.github.com>
2025-10-17 22:07:00 +0700

    Unused File Removal and Dark Mode support for Download Fragment's Icons

--- app/src/main/java/vn/edu/lianac/Download/DownloadFragment.java
+++ app/src/main/java/vn/edu/lianac/Download/DownloadFragment.java
@@ -38,8 +38,8 @@ public class DownloadFragment extends Fragment implements DownloadAdapter.Downlo
     private RecyclerView mRecyclerView;
 
     // For testing purposes, remove later
-    private EditText etPdfUrl;
-    private Button btnDownload;
+//    private EditText etPdfUrl;
+//    private Button btnDownload;
     // End of testing section
 
     private DownloadResultReceiver progressReceiver;
@@ -106,16 +106,16 @@ public class DownloadFragment extends Fragment implements DownloadAdapter.Downlo
         });
 
         // For testing purposes, remove later
-        etPdfUrl = view.findViewById(R.id.et_pdf_url);
-        btnDownload = view.findViewById(R.id.btn_download);
-
-        btnDownload.setOnClickListener(v -> {
-            String url = etPdfUrl.getText().toString();
-            if (!url.isEmpty()) {
-                mViewModel.fetchTitleAndAddDownload(url);
-                etPdfUrl.setText("");
-            }
-        });
+//        etPdfUrl = view.findViewById(R.id.et_pdf_url);
+//        btnDownload = view.findViewById(R.id.btn_download);
+//
+//        btnDownload.setOnClickListener(v -> {
+//            String url = etPdfUrl.getText().toString();
+//            if (!url.isEmpty()) {
+//                mViewModel.fetchTitleAndAddDownload(url);
+//                etPdfUrl.setText("");
+//            }
+//        });
         // End of testing section
     }
 
